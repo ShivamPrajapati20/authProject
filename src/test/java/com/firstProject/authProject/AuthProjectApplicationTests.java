c96ae24309bdcf8b8bc0618eeecb3c64e29de971
Shivam Prajapati <shivamp@fyndme.net>
2026-01-04 06:53:12 -0500

    Initial commit

---
+++ src/test/java/com/firstProject/authProject/AuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstProject.authProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
